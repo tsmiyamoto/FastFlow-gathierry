10d9688b4dccaa5c257215e99731d6bcf3875cc7
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2022-04-27 21:06:20 +0900

    拡張子のハードコーディングの訂正とファイル名の取得

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 from glob import glob
 
@@ -6,14 +7,24 @@ import torch.utils.data
 from PIL import Image
 from torchvision import transforms
 
+exts = ["jpg", "jpeg", "png", "bmp"]
+
 
 class MVTecDataset(torch.utils.data.Dataset):
     def __init__(self, root, category, input_size, is_train=True):
         self.image_transform = transforms.Compose([transforms.Resize(input_size), transforms.ToTensor()])
         if is_train:
-            self.image_files = glob(os.path.join(root, category, "train", "good", "*.png"))
+            self.image_files = list(
+                itertools.chain.from_iterable(
+                    [glob(os.path.join(root, category, "train", "good", f"*.{ext}")) for ext in exts]
+                )
+            )
         else:
-            self.image_files = glob(os.path.join(root, category, "test", "*", "*.png"))
+            self.image_files = list(
+                itertools.chain.from_iterable(
+                    [glob(os.path.join(root, category, "test", "*", f"*.{ext}")) for ext in exts]
+                )
+            )
 
         self.is_train = is_train
 
@@ -21,7 +32,11 @@ class MVTecDataset(torch.utils.data.Dataset):
         image_file = self.image_files[index]
         image = Image.open(image_file)
         image = self.image_transform(image)
-        return image
+        file_name = os.path.split(self.image_files[index])[1]
+        if self.is_train:
+            return image
+        else:
+            return image, file_name
 
     def __len__(self):
         return len(self.image_files)
